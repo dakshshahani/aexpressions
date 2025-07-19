db3748c6c134d3a3d1d276dc7871bae78c096cb1
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-20 02:03:35 +0300

    intro/navbar css fixed

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,11 +2,14 @@ import styles from '../styles/intro.module.css';
 
 export default function Intro() {
   return (
-    <div className={styles.intro}>
-      <h1>AExpressions</h1>
-      <h2>Here to help you with After Effects Math 🤓</h2>
-      <p className={styles.introText}>
-      Discover powerful expressions to automate animations, create complex effects, and streamline your workflow. From simple wiggles to advanced mathematical formulas, find exactly what you need to bring your compositions to life. </p>
+    <div className={styles.introContainer}>
+        <div className={styles.intro}>
+            <h1>AExpressions</h1>
+            <h2>Here to help you with After Effects Math 🤓</h2>
+            <p className={styles.introText}>
+            Discover powerful expressions to automate animations, create complex effects, and streamline your workflow. From simple wiggles to advanced mathematical formulas, find exactly what you need to bring your compositions to life. </p>
+        </div>
+
     </div>
   );
 }   
