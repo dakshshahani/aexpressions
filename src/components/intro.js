5a8421c1701892246206c3fcceaab6e66e732bcf
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-20 15:02:45 +0300

    intro css changed

--- src/components/intro.js
+++ src/components/intro.js
@@ -8,6 +8,9 @@ export default function Intro() {
             <h2>Here to help you with After Effects Math 🤓</h2>
             <p className={styles.introText}>
             Discover powerful expressions to automate animations, create complex effects, and streamline your workflow. From simple wiggles to advanced mathematical formulas, find exactly what you need to bring your compositions to life. </p>
+            <div className={styles.introButton}>
+                <button>Start Exploring</button>
+            </div>
         </div>
 
     </div>
