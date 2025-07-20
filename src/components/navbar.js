d2aefb010a769a92472c76e2b6b0afb5dbcbacfb
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-20 19:36:03 +0300

    fixed navbar bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,17 +4,15 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContent}>
-        <div className={styles.navbarLinks}>
-          <a href="#AExpressions" className={styles.navbarLink}>
-            AExpressions
-          </a>
-          <a href="#popularExpressions" className={styles.navbarLink}>
-            Popular Expressions
-          </a>
-          <a href="#chat" className={styles.navbarLink}>
-            Ask AI
-          </a>
-        </div>
+        <a href="#AExpressions" className={styles.navbarTitle}>
+          AExpressions
+        </a>
+        <a href="#popularExpressions" className={styles.navbarLink}>
+          Popular Expressions
+        </a>
+        <a href="#chat" className={styles.navbarLink}>
+          Ask AI
+        </a>
       </div>
     </nav>
   );
