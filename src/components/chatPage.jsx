43564af8fa6a0981ccb2d4935f9b6140354d36c3
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-20 18:31:00 +0300

    css for chat page

--- src/components/chatPage.jsx
+++ src/components/chatPage.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import styles from '../styles/chatPage.module.css';
+import React from "react";
+import styles from "../styles/chatPage.module.css";
 
 export default function ChatPage() {
   return (
-    <div className={styles.chatBox}>
+    <div className={styles.page}>
+      <h2 className={styles.pageTitle}>Chat with AExpressions</h2>
+      <div className={styles.chatBox}>
         <div className={styles.chatTextLeft}>
-        <h2>Can't find your expression? Ask AI to create it!</h2>
-        <p>Enter your expression and we'll create it for you!</p>
-        <form>
-            <input type='text' placeholder='camera shake wiggle expression..' />
-            <button type='submit'>Submit</button>
-            <button type='reset'>Reset</button>
-        </form>
+          <p>Enter your expression and we'll create it for you!</p>
+          <form>
+            <input type="text" placeholder="camera shake wiggle expression.." />
+           <div className={styles.buttons}>
+            <button type="submit">Submit</button>
+            <button type="reset">Reset</button>
+           </div>
+          </form>
         </div>
         <div className={styles.codeContainer}>
-        <p>Copy the code and paste it into your After Effects project.</p>
-        <div className={styles.code}>
+          <p>Copy the code and paste it into your After Effects project.</p>
+          <div className={styles.code}>
             <code>
-                <p>camera shake wiggle expression..</p>
+              <p>camera shake wiggle expression..</p>
             </code>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
