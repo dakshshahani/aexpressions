bab94769b9837750146628830497e137360efecf
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-22 20:37:38 +0300

    Card CSS

--- src/components/ExpandedCard.jsx
+++ src/components/ExpandedCard.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import styles from "../styles/ExpandedCard.module.css";
 import Button from "./Button";
+import close from "./close.svg";
 
 export default function ExpandedCard(props) {
   return (
     <div className={styles.overlay}>
-      <div className={styles.ExpandedCard}></div>
-      <Button text="Close" clickFunction={props.onClose} />
-      <div className={styles.cardContent}>
-        <h3 className={styles.cardTitle}>{props.title}</h3>
-        <div className={styles.codeContainer}>
-          <p className={styles.code}>{props.code}</p>
+      <div className={styles.ExpandedCard}>
+        <div className={styles.cardSection}>
+          <h2 className={styles.cardTitle}>
+            {props.data.icon} {props.data.title}
+          </h2>
+          <div className={styles.button} onClick={props.onClose}>
+            <img src={close} alt="close" />
+          </div>
         </div>
-        <h3 className="cardTitle">How it works</h3>
-        <p className="cardDescription">{props.explanation}</p>
-        <h3 className="cardTitle">Example</h3>
-        <p className="cardDescription">{props.example}</p>
-        <h3 className="cardTitle">Use Case</h3>
-        <p className="cardDescription">{props.useCase}</p>
+        <div className={styles.codeContainer}>
+          <code className={styles.code}>{props.data.code}</code>
+        </div>
+        <div className={styles.cardPart}>
+          <h3 className={styles.cardTitle}>Explanation</h3>
+          <p className={styles.cardDescription}>{props.data.explanation}</p>
+        </div>
+        <div className={styles.cardPart}>
+          <h3 className={styles.cardTitle}>Example</h3>
+          <p className={styles.cardDescription}>{props.data.example}</p>
+        </div>
+        <div className={styles.cardPart}>
+          <h3 className={styles.cardTitle}>Use Case</h3>
+          <p className={styles.cardDescription}>{props.data.useCase}</p>
+        </div>
       </div>
     </div>
   );
